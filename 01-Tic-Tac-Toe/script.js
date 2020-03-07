70ac8a3bff7c3331cf23953628e37e18a4f8f3ee
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-03-07 22:09:34 +0100

    Create the board (HTML) and style it.
    I've crated the board for game and added some styles, I've also created
    the basics of the script.

---
+++ 01-Tic-Tac-Toe/script.js
@@ -0,0 +1,13 @@
+// Catch DOM elements
+
+// Create the list of moves
+
+// Ascertain who is the winner
+
+// Allow to choose X or O
+  // if O - computer is doing the first move
+  // else - human
+
+// Moves
+
+// Event listeners
